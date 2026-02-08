7a64c56e99b98898761d038b2c06f73f775f8dd4
malachi Maenle <malachimaenle@gmail.com>
2026-02-08 14:32:47 +0800

    Fixing gearbound Typescript

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import {Component, signal} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
@@ -9,4 +11,14 @@ import {RouterOutlet} from '@angular/router';
 })
 export class App {
     protected readonly title = signal('foxlabs');
+    hideDefaultNav = false;
+
+    constructor(private router: Router) {
+        this.router.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe((event: any) => {
+                this.hideDefaultNav = event.url.includes('gearbound');
+            });
+    }
+
 }
